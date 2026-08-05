213e4255f18c806b6bd6c1fcf9bb36d98604be1f
spex72 <thorsten@kindermaenner.de>
2026-08-05 18:23:50 +0200

    - Alte tests entfernt

---
+++ app/src/test/java/de/kindermaenner/monatsblitz/ExampleTest.kt
@@ -0,0 +1,12 @@
+package de.kindermaenner.monatsblitz.infrastructure.api.dto
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class ExampleTest {
+
+    @Test
+    fun dummyTest() {
+        assertTrue(true)
+    }
+}

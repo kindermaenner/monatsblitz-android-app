57f4b4cf507ef7303fca49cfffb0bbcb364aa9c8
abraquax <nina@kindermaenner.de>
2026-08-08 23:49:18 +0200

    actions: use kover and sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,38 @@ plugins {
     alias(libs.plugins.kotlin.serialization) apply false
 
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.sonarqube)
+    alias(libs.plugins.kover)
+}
+
+sonarqube {
+    properties {
+        property("sonar.host.url", System.getenv("SONAR_HOST_URL") ?: "http://localhost:9000")
+        property("sonar.token", System.getenv("SONAR_TOKEN") ?: "")
+        property("sonar.projectKey", "monatsblitz-android")
+        property("sonar.projectName", "Monatsblitz Android")
+        property("sonar.sourceEncoding", "UTF-8")
+        property("sonar.android.lint.reportPaths", "app/build/reports/lint-results-debug.xml")
+        property("sonar.coverage.jacoco.xmlReportPaths", "${layout.buildDirectory.get().asFile}/reports/kover/report.xml")
+    }
+}
+
+dependencies {
+    kover(project(":app"))
+}
+
+kover {
+    reports {
+        filters {
+            excludes {
+                classes(
+                    "**/R.class", "**/R$*.class", "**/BuildConfig.*", "**/Manifest*.*",
+                    "android/**/*.*", "**/databinding/*", "**/androidx/*",
+                    "**/*MapperImpl*.*", "**/*Component*.*", "**/*BR*.*",
+                    "**/*Module*.*", "**/*Dagger*.*", "**/*Hilt*.*",
+                    "**/*_Factory*.*", "**/*_Provide*Factory*.*"
+                )
+            }
+        }
+    }
 }
